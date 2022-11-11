59ce79d06f204bfd11e14ae619f5ce3f267a70cc
hi098123 <46213700+hi098123@users.noreply.github.com>
2022-11-11 21:54:09 +0900

    Update test.js

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,32 @@
 const http = require('http');
 const https = require('https');
 //https://luckyyowu.tistory.com/346
-export default (req, res) => {
-  res.status(200).send(JSON.stringify(req.ip))
+export default (req_, res_) => {
+  //res_.status(200).send(JSON.stringify(req_.ip))
+  var options = {
+    host: 'www.google.com',
+    path: '/index.html'
+  };
+
+  var req = http.get(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+    // Buffer the body entirely for processing as a whole.
+    var bodyChunks = [];
+    res.on('data', function(chunk) {
+      // You can process streamed parts here...
+      bodyChunks.push(chunk);
+    }).on('end', function() {
+      var body = Buffer.concat(bodyChunks);
+      console.log('BODY: ' + body);
+      res_.status(200).send(body)
+      // ...and/or process the entire body here.
+    })
+  });
+
+  req.on('error', function(e) {
+    //console.log('ERROR: ' + e.message);
+    res_.status(200).send(e.message)
+  });
 }
