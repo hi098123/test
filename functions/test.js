fd314ecb4aa6800c7a768bbd641b31ffb442d236
hi098123 <46213700+hi098123@users.noreply.github.com>
2022-11-11 21:50:19 +0900

    Update test.js

--- functions/test.js
+++ functions/test.js
@@ -1,3 +1,6 @@
+const http = require('http');
+const https = require('https');
+//https://luckyyowu.tistory.com/346
 export default (req, res) => {
-  res.status(200).send(JSON.stringify(req.headers))
+  res.status(200).send(JSON.stringify(req.ip))
 }
